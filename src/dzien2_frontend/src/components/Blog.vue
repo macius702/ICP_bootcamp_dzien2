3e59ed375d5d5a858dab4486967f84cbd8782e4a
Matiki <maciej.tomasz.krzeminski@gmail.com>
2024-06-26 00:27:54 +0200

    Handle display and Input in Vue component

--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -1,5 +1,33 @@
 <template>
     <div>
+        <button @click="pobierzWpisy">refresh</button>
         siema blog!!
+        <input v-model="nowyBlog" type="text">
+        <button @click="dodajWpisy">dodaj</button>
+        {{ wpisy }}
     </div>
 </template>
+
+<script>
+import { dzien2_backend } from 'declarations/dzien2_backend/index';
+export default {
+    data() {
+        return {
+            wpisy: []
+        }
+    },
+    methods: {
+        async pobierzWpisy() {
+            let allTexts = await dzien2_backend.get_all_texts();
+            console.log(allTexts);
+            this.wpisy = allTexts;           
+         },
+         async dodajWpisy() {
+            await dzien2_backend.add_text(this.nowyBlog);
+            this.pobierzWpisy();
+         }
+         
+    },
+ 
+}
+</script>
