8fb9171568136a9542868f24a7e8a403f79b9a5c
Matiki <maciej.tomasz.krzeminski@gmail.com>
2024-06-26 19:45:34 +0200

    Add delete button

--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -1,19 +1,19 @@
 <template>
     <div>
-        <h2 class="text-blue-600"> Wpisy na bloga</h2>
+        <h2 class="text-blue-600">Wpisy na bloga:</h2>
         <div class="w-100 flex flex-row-reverse">
-            <button @click="pobierzWpisy" class="bg-blue-600 rounded text-white 
-            p-4">refresh</button>
+            <button @click="pobierzWpisy" class="bg-blue-600 rounded text-white p-4">refresh</button>
         </div>
-
         <div class="grid mx-6 gap-4 my-4">
-            <div v-for="wpis in wpisy" class="drop-shadow-x1 bg-stone-300 p-4">
+            <div v-for="(wpis, index) in wpisy" class="drop-shadow-xl bg-stone-300 p-4">
+                <p>id: {{ index }}</p>
                 <p>{{ wpis }}</p>
+                <button  class="bg-blue-600 rounded text-white p-4" @click="deleteWpis(index)">usun</button>
             </div>
         </div>
         <div class="flex justify-center flex-col">
             <input v-model="nowyBlog" class="border-2 border-blue-600 p-4" type="text">
-            <button @click="dodajWpisy" class="bg-blue-600 rounded-sm text-white p-4">dodaj</button>
+            <button  class="bg-blue-600 rounded text-white p-4" @click="dodajWpisy">dodaj</button>
         </div>
     </div>
 </template>
@@ -29,16 +29,19 @@ export default {
         }
     },
     methods: {
+        async dodajWpisy() {
+            await dzien2_backend.add_text(this.nowyBlog);
+            this.pobierzWpisy();
+        },
+        async deleteWpis(index) {
+            await dzien2_backend.delete_text(index);
+            await this.pobierzWpisy();
+        },
         async pobierzWpisy() {
             let allTexts = await dzien2_backend.get_all_texts();
             console.log(allTexts);
             this.wpisy = allTexts;
         },
-        async dodajWpisy() {
-            await dzien2_backend.add_text(this.nowyBlog);
-            this.pobierzWpisy();
-        }
-
     },
     async mounted() {
         this.pobierzWpisy()
