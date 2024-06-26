65339527cffbcf6413e1faf8fb0a855d4389a1bb
Matiki <maciej.tomasz.krzeminski@gmail.com>
2024-06-26 19:06:25 +0200

    Style

--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -1,12 +1,20 @@
 <template>
     <div>
         <h2 class="text-blue-600"> Wpisy na bloga</h2>
-        <button @click="pobierzWpisy">refresh</button>
-        <div v-for="wpis in wpisy">
-            <p>{{wpis}}</p>
+        <div class="w-100 flex flex-row-reverse">
+            <button @click="pobierzWpisy" class="bg-blue-600 rounded text-white 
+            p-4">refresh</button>
         </div>
-        <input v-model="nowyBlog" type="text">
-        <button @click="dodajWpisy">dodaj</button>
+
+        <div class="grid mx-6 gap-4 my-4">
+            <div v-for="wpis in wpisy" class="drop-shadow-x1 bg-stone-300 p-4">
+                <p>{{ wpis }}</p>
+            </div>
+        </div>
+        <div class="flex justify-center flex-col">
+            <input v-model="nowyBlog" type="text">
+            <button @click="dodajWpisy" class="bg-blue-600 rounded-sm text-white p-4">dodaj</button>
+        </div>
     </div>
 </template>
 
@@ -24,17 +32,17 @@ export default {
         async pobierzWpisy() {
             let allTexts = await dzien2_backend.get_all_texts();
             console.log(allTexts);
-            this.wpisy = allTexts;           
-         },
-         async dodajWpisy() {
+            this.wpisy = allTexts;
+        },
+        async dodajWpisy() {
             await dzien2_backend.add_text(this.nowyBlog);
             this.pobierzWpisy();
-         }
-         
+        }
+
     },
-    async mounted(){
+    async mounted() {
         this.pobierzWpisy()
     }
- 
+
 }
 </script>
